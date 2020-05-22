3d17a001874e9cbab000738ee7236d5ee36f5962
Deepak Singh <deepakanil@gmail.com>
2020-05-22 14:12:56 +0200

    Fixed VM argument issue

--- src/main/java/guru/sfg/beer/order/service/BeerOrderServiceApplication.java
+++ src/main/java/guru/sfg/beer/order/service/BeerOrderServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BeerOrderServiceApplication {
 
-    public static void main(String[] args) {
+    public static void main(String ...args) {
         SpringApplication.run(BeerOrderServiceApplication.class, args);
     }
 
