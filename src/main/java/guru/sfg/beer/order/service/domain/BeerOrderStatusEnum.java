9a36427a60257c2b7a4be1631253befefbf1633a
Deepak Singh <deepakanil@gmail.com>
2020-05-24 22:54:11 +0200

    Added enums

--- src/main/java/guru/sfg/beer/order/service/domain/BeerOrderStatusEnum.java
+++ src/main/java/guru/sfg/beer/order/service/domain/BeerOrderStatusEnum.java
@@ -15,8 +15,9 @@
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package guru.sfg.beer.order.service.web.model;
+package guru.sfg.beer.order.service.domain;
 
-public enum OrderStatusEnum {
-    NEW, READY, PICKED_UP
+public enum BeerOrderStatusEnum {
+    NEW, VALIDATED, VALIDATION_EXCEPTION, ALLOCATED, ALLOCATION_EXCEPTION,
+    PENDING_INVENTORY, PICKED_UP, DELIVERED, DELIVERY_EXCEPTION
 }
